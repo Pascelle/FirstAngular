0594d6de443216f759f6dc5a063c926884d6266a
Pascelle <pascelle.saintlaiurent@gmail.com>
2017-01-18 17:50:47 -0500

    before gym

--- app/courses.component.js
+++ app/courses.component.js
@@ -1,18 +1,40 @@
-System.register([], function(exports_1, context_1) {
+//this is a decorator called component imported from module core
+System.register(['angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
     var CoursesComponent;
     return {
-        setters:[],
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
         execute: function() {
+            //decorators are functions so we need to call them using @
+            //There is an object within the function
             CoursesComponent = (function () {
                 function CoursesComponent() {
                 }
+                CoursesComponent = __decorate([
+                    core_1.Component({
+                        selector: 'courses',
+                        //when angular sees an element that matches this css selector, it will create an instance of our component in the host element 
+                        template: ''
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], CoursesComponent);
                 return CoursesComponent;
             }());
-            exports_1("CoursesComponent", CoursesComponent); /* this creates a class
-                
-            }
+            exports_1("CoursesComponent", CoursesComponent);
         }
     }
 });
--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,3 +1,20 @@
-export class CoursesComponent {  /* this creates a class */
+//this is a decorator called component imported from module core
+
+import {Component} from 'angular2/core'
+
+//decorators are functions so we need to call them using @
+//There is an object within the function
+@Component({
+	selector:'courses'
+	//when angular sees an element that matches this css selector, it will create an instance of our component in the host element 
+	template: ''
 	
-}
+	
+})
+
+
+//this creates a class
+export class CoursesComponent {  
+
+}								
+ 
